7503b25436e01abc51e972c7e54a375c42ece3ee
LiderMorti00 <lidermorti@gmail.com>
2025-03-19 01:29:00 +0100

    Arreglos

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1153,6 +1153,7 @@ EventScript_VsSeekerChargingDone::
 	.include "data/scripts/follower.inc"
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
+	.include "data/scripts/dexnav.inc"
 	.include "data/maps/TwinleafTown/scripts.inc"
 	.include "data/maps/SandgemTown/scripts.inc"
 	.include "data/maps/JubilifeCity/scripts.inc"
