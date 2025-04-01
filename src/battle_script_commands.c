f2818c1fd4f3ec30f872ed9f9346142e46380d68
StrangeQuark <zacharyzirkle@hotmail.com>
2025-04-01 14:08:07 -0500

    Remove change to halve experience gained in battles

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -3293,7 +3293,7 @@ static void Cmd_getexp(void)
                     viaExpShare++;
             }
 
-            calculatedExp = (gSpeciesInfo[gBattleMons[gBattlerFainted].species].expYield * gBattleMons[gBattlerFainted].level / 7) / 2;//Reduce calulated exp by half
+            calculatedExp = (gSpeciesInfo[gBattleMons[gBattlerFainted].species].expYield * gBattleMons[gBattlerFainted].level / 7);
 
             if (viaExpShare) // at least one mon is getting exp via exp share
             {
