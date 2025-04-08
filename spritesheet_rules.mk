2d83f4e81aaea59bf6a1ec0a790c69d6c5249f3e
StrangeQuark <zacharyzirkle@hotmail.com>
2025-04-08 14:58:26 -0500

    Add Delia (Kanto mom)

--- spritesheet_rules.mk
+++ spritesheet_rules.mk
@@ -688,3 +688,6 @@ $(OBJEVENTGFXDIR)/people/prof_oak.4bpp: %.4bpp: %.png
 
 $(OBJEVENTGFXDIR)/people/prof_elm.4bpp: %.4bpp: %.png
 	$(GFX) $< $@ -mwidth 2 -mheight 4
+
+$(OBJEVENTGFXDIR)/people/delia.4bpp: %.4bpp: %.png
+	$(GFX) $< $@ -mwidth 2 -mheight 4
