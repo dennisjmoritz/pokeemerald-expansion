d7384d66be32360b646c6a5ab3532e5371107b01
DJ Moritz <dennis.j.moritz@gmail.com>
2025-09-14 15:37:42 -0600

    Compiles with most map scripts added back. Intro scripts for other regions removed as I am not using them. Boats should nearly work but may have sonme issues.

--- include/constants/event_objects.h
+++ include/constants/event_objects.h
@@ -247,10 +247,15 @@
 #define OBJ_EVENT_GFX_OW_MON                     240
 #define OBJ_EVENT_GFX_LIGHT_SPRITE               241
 
+#define OBJ_EVENT_GFX_DELIA                      242
+#define OBJ_EVENT_GFX_PROF_OAK                   243
+#define OBJ_EVENT_GFX_GRACE                      244
+#define OBJ_EVENT_GFX_PROF_ELM                   245
+
 // NOTE: The maximum amount of object events has been expanded from 255 to 65535.
 // Since dynamic graphics ids still require at least 16 free values, the actual limit
 // is 65519, but even considering follower Pokémon, this should be more than enough :)
-#define NUM_OBJ_EVENT_GFX                        242
+#define NUM_OBJ_EVENT_GFX                        246
 
 
 // These are dynamic object gfx ids.
